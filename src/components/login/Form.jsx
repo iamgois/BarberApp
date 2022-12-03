041b08c1d6cf09b82932cbab0148bd4eb4a8a5ea
iamgois <thiagogoisjunior@gmail.com>
2022-12-03 17:52:14 -0300

    login and register form

--- src/components/login/Form.jsx
+++ src/components/login/Form.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
 
 function Form() {
+
+    function login(e){
+        e.preventDefault()
+
+        let email = localStorage.getItem('email')
+        let password = localStorage.getItem('password')
+
+        let isEmail = document.getElementById('email').value
+        let isPassword = document.getElementById('password').value
+
+        if(isEmail === email && isPassword === password){
+            return console.log('Email e Senha Correto!')
+        }else{
+            return console.log('Credenciais erradas!')
+        }
+    }
+
     return (
         <div>
             <form className='flex flex-col mt-20 items-center'>
-                <input required type="email" name="email" id="email" placeholder='Digite seu email' className='bg-transparent border-[3px] border-[#E95401] w-80 h-12 rounded-2xl text-center text-white font-bold'/>
+                <input  required type="email" name="email" id="email" placeholder='Digite seu email' className='bg-transparent border-[3px] border-[#E95401] w-80 h-12 rounded-2xl text-center text-white font-bold'/>
                 <input required type="password" name="password" id="password" placeholder='Digite sua senha' className='bg-transparent border-[3px] border-[#E95401] w-80 h-12 rounded-2xl text-center mt-2 text-white font-bold'/>
 
-                <button type="submit" className='text-white font-bold bg-[#E95401] w-48 h-10 rounded-[10px] mt-28 border-2 border-[#E95401] hover:bg-transparent hover:border-[#E95401] hover:border-2 transition duration-300 ease-in-out'>Entrar</button>
+                <button type="submit" className='text-white font-bold bg-[#E95401] w-48 h-10 rounded-[10px] mt-28 border-2 border-[#E95401] hover:bg-transparent hover:border-[#E95401] hover:border-2 transition duration-300 ease-in-out' onClick={login}>Entrar</button>
                 <a href='#' className='text-white font-bold text-xs mt-2 hover:text-slate-400 transition ease-in-out duration-300'>Esqueci minha senha</a>
             </form>
         </div>
--- src/components/register/Form.jsx
+++ src/components/register/Form.jsx
@@ -1,6 +1,19 @@
 import React from 'react'
 
 function Form() {
+
+    function register(e){
+        e.preventDefault()
+
+        let name = document.getElementById('name').value
+        let email = document.getElementById('email').value
+        let password = document.getElementById('password').value
+
+        localStorage.setItem('name', name)
+        localStorage.setItem('email', email)
+        localStorage.setItem('password', password)
+    }
+
     return (
         <div>
             <form className='flex flex-col mt-1 items-center'>
@@ -8,16 +21,16 @@ function Form() {
 
                 <input required type="email" name="email" id="email" placeholder='Digite seu email' className='bg-transparent border-[3px] border-[#E95401] w-80 h-12 rounded-2xl text-center text-white font-bold m-1'/>
 
-                <input required type="text" name="password" id="password" placeholder='Crie uma senha' className='bg-transparent border-[3px] border-[#E95401] w-80 h-12 rounded-2xl text-center text-white font-bold m-1'/>
+                <input required type="password" name="password" id="password" placeholder='Crie uma senha' className='bg-transparent border-[3px] border-[#E95401] w-80 h-12 rounded-2xl text-center text-white font-bold m-1'/>
 
-                <input  required type="text" name="password" id="password" placeholder='Repita sua senha' className='bg-transparent border-[3px] border-[#E95401] w-80 h-12 rounded-2xl text-center text-white font-bold m-1'/>
+                <input  required type="password" name="password" id="rePassword" placeholder='Repita sua senha' className='bg-transparent border-[3px] border-[#E95401] w-80 h-12 rounded-2xl text-center text-white font-bold m-1'/>
 
                 <div className='flex items-center'>
                     <input  required className='transition duration-200 ease-in-out cursor-pointer appearance-none w-5 h-5 border-2 border-[#E95401] checked:bg-[#E95401] rounded-full checked:text-white' type="checkbox" name="termos" id="termos" />
                     <p className='text-white pl-2 text-xs'>Aceito os termos de uso e diretrizes do app</p>
                 </div>
 
-                <button type="submit" className='text-white font-bold bg-[#E95401] w-48 h-10 rounded-[10px] border-2 border-[#E95401] hover:bg-transparent hover:border-[#E95401] hover:border-2 transition duration-300 ease-in-out mt-2'>Entrar</button>
+                <button type="submit" onClick={register} className='text-white font-bold bg-[#E95401] w-48 h-10 rounded-[10px] border-2 border-[#E95401] hover:bg-transparent hover:border-[#E95401] hover:border-2 transition duration-300 ease-in-out mt-2'>Entrar</button>
 
                 <a href='#' className='text-white font-bold text-xs mt-2 hover:text-slate-400 transition ease-in-out duration-300'>Esqueci minha senha</a>
             </form>
